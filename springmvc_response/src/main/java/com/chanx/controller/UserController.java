72503e16dfbe472d0865909756fcaa390baf9f32
ChanX <821602490@qq.com>
2020-01-07 14:35:46 +0800

    springmvc

--- springmvc_response/src/main/java/com/chanx/controller/UserController.java
+++ springmvc_response/src/main/java/com/chanx/controller/UserController.java
@@ -3,7 +3,15 @@ package com.chanx.controller;
 import com.chanx.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/user")
@@ -29,10 +37,74 @@ public class UserController {
 
     /**
      * 返回值为void
+     * 请求转发一次请求，不用编写项目名称。
      * @return
      */
     @RequestMapping("/testVoid")
-    public void testVoid() {
+    public void testVoid(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("testVoid执行了");
+        // 编写请求转发
+//        request.getRequestDispatcher("/WEB-INF/pages/success.jsp").forward(request, response);
+
+        // 重定向
+//        response.sendRedirect(request.getContextPath() + "/index.jsp");
+
+        // 设置中文乱码
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        // 直接响应
+        response.getWriter().println("你好");
+        return;
+    }
+
+    /**
+     * ModelAndView
+     * @return
+     */
+    @RequestMapping("/testModelAndView")
+    public ModelAndView testModelAndView() {
+        // 创建ModelAndView对象
+        ModelAndView mv = new ModelAndView();
+        System.out.println("testModelAndView");
+        // 模拟从数据库中查询User对象
+        User user = new User();
+        user.setUsername("晓峰");
+        user.setPassword("456");
+        user.setAge(20);
+        // 把user存储到mv对象中，也会把user对象存入request对象
+        mv.addObject("user", user);
+        // 跳转到哪个页面,调用springmvc的视图解析器
+        mv.setViewName("success");
+        return mv;
+    }
+
+    /**
+     * 使用关键字转发或重定向
+     * @param
+     * @return
+     */
+    @RequestMapping("/testForwardOrRedirect")
+    public String testForwardOrRedirect() {
+        System.out.println("testForwardOrRedirect执行了");
+        // 请求转发
+//        return "forward:/WEB-INF/pages/success.jsp";
+
+        //重定向
+        return "redirect:/index.jsp";
+    }
+
+    /**
+     * 模拟异步请求响应
+     */
+    @RequestMapping("/testAjax")
+    public @ResponseBody User testAjax(@RequestBody User user) {
+        System.out.println("testAjax执行了");
+        // 客户端发送ajax请求，传的是json字符串，后端把json字符串封装到user对象中
+        System.out.println(user);
+        // 响应，模拟查询数据库
+        user.setUsername("haha");
+        user.setAge(40);
+        return user;
     }
 }
--- springmvc_response/src/main/webapp/response.jsp
+++ springmvc_response/src/main/webapp/response.jsp
@@ -9,6 +9,33 @@
 <html>
 <head>
     <title>Title</title>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+
+    <script>
+        // 页面加载，绑定单击事件
+        $(function () {
+            $("#btn").click(function () {
+                // alert("hello btn");
+                // 发送ajax请求
+                $.ajax({
+                    // 编写json格式，设置属性和值
+                    url:"user/testAjax",
+                    contentType:"application/json;charset=UTF-8",
+                    data:'{"username":"王小明","password":"123","age":30}',
+                    dataType:"json",
+                    type:"post",
+                    success:function (data) {
+                        // data是服务器端响应的json数据，进行解析
+                        alert(data);
+                        alert(data.username);
+                        alert(data.password);
+                        alert(data.age);
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
 
@@ -17,5 +44,16 @@
     <br>
 
     <a href="user/testVoid">testVoid</a>
+
+    <br>
+
+    <a href="user/testModelAndView">testModelAndView</a>
+
+    <br>
+
+    <a href="user/testForwardOrRedirect">testForwardOrRedirect</a>
+
+    <br>
+    <button id="btn">发送ajax的请求</button>
 </body>
 </html>
